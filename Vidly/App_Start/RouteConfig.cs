0f30e2f20b57e62b61f87c6cb36d8f700bb9f178
Giuseppe <giuseppe.cristaudo@gmail.com>
2017-02-23 15:13:47 +0100

    aggiornamento al 20170223

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,40 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();  // permette di mappare gli attributi definiti direttamente dentro i vari Controller (vedi commento qui sotto).
+
+
+
+
+
+            /*
+             * Poichè, ogni volta che si cambia nome alla Action del controller, sarebbe necessario tornare qui sul RouteConfig e andare a cambiare manualmente la relativa action in modo che abbia lo stesso nome,
+             * nelle ultime versioni di MVC, a partire da MVC5, microsoft ha introdotto un altro modo di creare un CustomLayout.
+             * Questo nuovo metodo prevede l'uso di un attributo sopra la corrispondente Action, da inserire direttamente dentro il Controller (vedi  la action ByReleaseYear dentro MoviesController).
+             * Qui nel RouteConfig basterà inserire:  
+             * 
+             *     routes.MapMvcAttributeRoutes();
+             * 
+             * Questo nuovo metodo permette inoltre di avere molto meno codice dentro il RouteConfig, mantenendolo più pulito.
+            */
+
+            // *******************************************************************************************************************************************************
+            //              ### Questo codice non serve più perchè da MVC5 in poi il routing viene definito tramite attributi nel Controller stesso ###
+
+            //// Definisco un CustomLayout
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}" } // regular expression per indicare che l'anno deve avere 4 numeri (diit) e il mese 2 numeri
+            //);
+
+            // *******************************************************************************************************************************************************
+
+
+
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random  ()
+        {
+            /*
+            La classe AcionResult può restituire diversi Type, ognuno dei quali ha un suo Helper Method. Esempio:
+            Type                    |   Helper Method
+            ViewResult              |   View()
+            PartialViewResult       |   PartialView()
+            ContentResult           |   Content()
+            RedirectReult           |   Redirect()
+            RedirectToRouteResult   |   RedirectToAction()
+            JsonResult              |   Json()
+            FileResult              |   File()
+            HttpNotFoundResult      |   HttpNotFound()
+            EmptyResult             |
+
+            */
+            var movie = new Movie() { Name = "Shrek!" };
+
+            ViewData["Movie"] = movie;  // Ogni controller ha una proprietàchiamata ViewData che è di tipo ViewDataDictionary
+            // return View(movie);  // E' un helper method ereditato dalla classe base Controller. Questo metodo permette inoltre di creare facilmente una ViewResult. Alla View viene passato un Model (movie in questo caso) come parametro
+            return View();
+
+        } 
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;  // Se non assegno alcun valore a pageIndex, di default verrà assegnato 1
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";    // Se non assegno alcun valore a sortBy, di default verrà assegnato "Name"
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]  // Attributo che permette di definire il Routing sostituendo di fatto il dover scrivere codiceB dentro RouteConfig
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    } 
+}
