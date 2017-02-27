a8037848cae521a1602273819abd49b3ef9d46f0
Giuseppe <giuseppe.cristaudo@gmail.com>
2017-02-27 13:02:21 +0100

    Fine sezione 2 - Lezione 19

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Vidly.Models;
 using Vidly.ViewModels;
 
@@ -10,27 +11,64 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+       
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = new List<Customer> // Lista di Customers
-            {
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 2" },
-                new Customer { Name = "Customer 3" },
-                new Customer { Name = "Customer 4" },
-                new Customer { Name = "Customer 5" }
-            };
-
-            /* L'oggetto "customers" appena creato verrà quindi passato alla view result.
+            /* L'oggetto "customers" verrà quindi passato alla view result.
                Creo quindi un'istanza dell'oggetto view model */
             var viewModel = new IndexCustomerViewModel
             {
-                Customers = customers // Inizializzo la proprietà Customers della ViewModel passandogli la lista di customers
+                Customers = GetCustomers() // Inizializzo la proprietà Customers della ViewModel passandogli la lista di customers
             };
 
 
             return View(viewModel); // Verificare sempre che nella View sia definito il model corretto. (In questo caso verificare nella View Index.cshtml)
+        }
+
+
+
+
+        [Route("customers/details/{id}")]  // Attributo che permette di definire il Routing sostituendo di fatto il dover scrivere codice dentro RouteConfig
+        public ActionResult Details(int Id)
+        {
+            var details = GetCustomer(Id);
+            return View(details);
+        }
+
+
+
+
+        // Get All Customers
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id=0, Name = "Customer 1" },
+                new Customer { Id=1, Name = "Customer 2" },
+                new Customer { Id=2, Name = "Customer 3" },
+                new Customer { Id=3, Name = "Customer 4" },
+                new Customer { Id=4, Name = "Customer 5" }
+            };
+        }
+
+
+
+        // Get Customer by Id
+        private Customer GetCustomer(int Id)
+        {
+            List<Customer> allCustomers = GetCustomers();
+
+            foreach (Customer customer in allCustomers)
+            {
+                if (customer.Id == Id)
+                {
+                    return customer;
+                    break;
+                }
+            }
+
+            return null;
         }
     }
 }
